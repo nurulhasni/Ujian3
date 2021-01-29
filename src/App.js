4cd2261f9d42b83e5fd561000c6dee8ff0e51cb2
nurulhasni <hasninurul23@gmail.com>
2021-01-29 16:55:39 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
+ 
 function App() {
+  const [inputList, setInputList] = useState([{ type: "", soal: "", a: "", b: "", c: "", d: "", kunci: "", score:"" }]);
+
+  const handleInputChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...inputList];
+    list[index][name] = value;
+    setInputList(list);
+  };
+
+  const handleAddClick = () => {
+    setInputList([...inputList, { type: "", soal: "", a: "", b: "", c: "", d: "", kunci: "", score: "" }]);
+  };
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {inputList.map((x, i) => {
+        return (
+          <div>
+            <select name="type"
+              value={x.type}
+              onChange={e => handleInputChange(e, i)}>
+                <option>Pilih</option>
+               <option value="radio">Radio</option>
+               <option value="checkbox">Checkbox</option>
+               <option value="textarea">Textarea</option>
+            </select><br/>
+            soal :<br/>
+            <input
+              name="soal"
+              value={x.soal}
+              onChange={e => handleInputChange(e, i)}
+            /><br/>
+            jawab a :<br/>
+            <input
+              name="a"
+              value={x.a}
+              onChange={e => handleInputChange(e, i)}
+            /> <br/>
+             jawab b :<br/>
+            <input
+              name="b"
+              value={x.b}
+              onChange={e => handleInputChange(e, i)}
+            /> <br/>
+             jawab c :<br/>
+             <input
+              name="c"
+              value={x.c}
+              onChange={e => handleInputChange(e, i)}
+            /> <br/>
+             jawab d :<br/>
+             <input
+              name="d"
+              value={x.d}
+              onChange={e => handleInputChange(e, i)}
+            /> <br/>
+            kunci : <br/>
+             <input
+              name="kunci"
+              value={x.kunci}
+              onChange={e => handleInputChange(e, i)}
+            /> <br/>
+            score : <br/>
+             <input
+              name="score"
+              value={x.score}
+              onChange={e => handleInputChange(e, i)}
+            /> 
+            <div>
+            <br/> <button onClick={handleAddClick}>Submit</button>
+            </div>
+          </div>
+        );
+      })}
+      <div style={{ marginTop: 20 }}>{JSON.stringify(inputList)}</div>
     </div>
   );
 }
-
+ 
 export default App;
